863fd4798a89168d9d5b7ef07a4dddab34fd0ef6
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-27 22:54:47 +0100

    add category to show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,9 +5,12 @@
         <h2 class="card-title">{{$post->title}}</h2>
         <img class="" src="{{$post->image}}" alt="{{$post->title}}">
         <h3 class="my-3">{{$post->sub_title}}</h3>
+        <h4>{{$post->category->name}}</h4>
         <div>
             <p class="card-text">{{$post->body}}</p>
         </div>
+
+
     </div>
 
 @endsection
--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -5,9 +5,17 @@
         <h2 class="card-title">{{$post->title}}</h2>
         <img class="" src="{{$post->image}}" alt="{{$post->title}}">
         <h3 class="my-3">{{$post->sub_title}}</h3>
+        <h4>
+            Category: {{ $post->category != null ? $post->category->name : 'Uncategorized' }}
+        </h4>
         <div>
             <p class="card-text">{{$post->body}}</p>
         </div>
+        @auth
+        <div class="actions">
+            <a href="#">Back to Admin</a>
+        </div>
+        @endauth
     </div>
 
 @endsection
