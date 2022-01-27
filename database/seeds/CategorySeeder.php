61e2406a6c8080fceae185437dbb80cd674cf4e1
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-27 20:47:02 +0100

    add category table migration, add category id column

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $categories = ['Programming', 'Automation', 'Web design', 'Best Practices'];
+        foreach ($categories as $category) {
+            $cate = new Category();
+            $cate->name = $category;
+            $cate->slug = Str::slug($category);
+            $cate->save();
+
+
+        }
+    }
+}
