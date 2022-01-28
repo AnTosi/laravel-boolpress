b40b9039511a5bc4bc026aa3d0e1683b082e766f
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-28 16:51:07 +0100

    change postcontroller to pass categories

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class PostController extends Controller
 {
@@ -16,7 +17,8 @@ class PostController extends Controller
     {
         //
         $posts = Post::orderByDesc('id')->paginate(9);
-        return view('guest.posts.index', compact('posts')); 
+        $categories = Category::all();
+        return view('guest.posts.index', compact('posts', 'categories')); 
     }
 
 
