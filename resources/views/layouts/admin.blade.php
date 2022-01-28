61c46c8990ab54d579ee2c755ebda1ec0d0943cc
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-28 17:01:16 +0100

    sidebar put in a partials part 2 and fix typo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -55,11 +55,11 @@
   
   <div class="container-fluid">
     <div class="row">
-      
+      @include('partials.admin.sidebar')
   
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            @yield('content')
-        </main>
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+          @yield('content')
+      </main>
     </div>
   </div>
 
