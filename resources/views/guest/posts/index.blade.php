851890d339181019dbfffd2bde3110e2c496add8
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-28 16:52:50 +0100

    add aside with categories

--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="d-flex">
     <div class="container">
         <div class="row gy-3">
             @foreach ($posts as $post)
                 <div class="col-md-4">
                     <div class="card">
-                        <img class="card-img-top" src="{{$post->image}}" alt="{{$post->title}}">
+                        <img class="card-img-top" height="165" src="{{$post->image}}" alt="{{$post->title}} image">
                         <div class="card-body">
                             <h4 class="card-title">{{$post->title}}</h4>
                             <p class="card-text"> {{$post->sub_title}}</p>
@@ -17,9 +18,25 @@
                 
             @endforeach
             
-        </div>
-        <div class="mt-3">
-            {{$posts->links()}}
+            <div class="mt-3">
+                {{$posts->links()}}
+            </div>
         </div>
     </div>
+        <aside class="w-25 ms-3 me-auto">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Categories:</h3>
+                </div>
+                <ul class="list-group list-group-flush">
+                    @forelse ($categories as $category)
+                        <li class="list-group-item"><a href="{{route('categories.posts', $category->slug)}}">{{$category->name}}</a></li>
+                    @empty
+                        No categories found
+                    @endforelse
+                </ul>
+            </div>
+        </aside>
+
+</div>
 @endsection
