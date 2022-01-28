a76a49560424bcfc1032b73702ae4a058557fcc6
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-28 17:05:28 +0100

    remove span feather (image) from sidebar, add bookmark icon

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -3,32 +3,28 @@
       <ul class="nav flex-column">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">
-            <span data-feather="home"></span>
             <i class="fas fa-tachometer-alt"></i> Dashboard
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">
-            <span data-feather="file"></span>
             Orders
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{route('admin.products.index')}}">
-            <span data-feather="shopping-cart"></span>
             Products
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{route('admin.posts.index')}}">
-            <span data-feather="users"></span>
             <i class="fas fa-thumbtack"></i> Posts
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{route('admin.categories.index')}}">
-            <span data-feather="users"></span>
-            Customers
+            <i class="fas fa-bookmark"></i>
+            Categories
           </a>
         </li>
 
