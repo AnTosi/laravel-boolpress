7d4a1263cab6c4973ba0224688a18573450ac274
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-29 17:46:07 +0100

    add tag multiselect to edit

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -49,6 +49,17 @@
         </div>
 
         <div class="mb-3">
+            <label for="tags" class="form-label">Tags</label>
+            <select multiple class="form-select" name="tags[]" id="tags">
+            <option disabled>Select all tags</option>
+            @foreach ($tags as $tag)
+                <option value="{{$tag->id}}">{{$tag->name}}</option>
+            @endforeach
+            
+            </select>
+        </div>
+        
+        <div class="mb-3">
             <label for="body" class="form-label">Text</label>
             <textarea class="form-control @error('body') is-invalid @enderror" name="body" id="body" rows="6" aria-describedby="bodyHelper">{{$post->body}}</textarea>
             <small id="bodyHelper" class="text-muted">Type the post text</small>
