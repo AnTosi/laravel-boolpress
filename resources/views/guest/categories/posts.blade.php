fa0fb3654bda9ef8e01f5965eabff1ccd5a01754
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-29 17:12:05 +0100

    add forelse loop to display tags

--- resources/views/guest/categories/posts.blade.php
+++ resources/views/guest/categories/posts.blade.php
@@ -4,6 +4,7 @@
     <div class="p-5 bg-dark text-white">
         <div class="container">
             <h1 class="display-3">Category: {{$category->name}}</h1>
+            <p>All posts with the {{$category->name}} tag</p>
         </div>
     </div>
 
@@ -17,6 +18,18 @@
                         <div class="card-body">
                             <h4 class="card-title">{{$post->title}}</h4>
                             <p class="card-text">{{$post->sub_title}}</p>
+                            <p class="category">
+                                @if($post->tags)
+                                <p> Tags:
+                                    @forelse ($post->tags as $tag)
+                                        <span class="tag mx-1"><a href="{{route('tags.posts', $tag->slug)}}">{{$tag->name}}</a></span>
+                                    @empty
+                                        <span>Not tagged</span>
+                                    @endforelse
+                                </p>                
+                                @endif
+                            </p>
+                            </p>
                         </div>
                     </div>
                 </a>                
