11ff89891a5e28eb6a5fad834566c784180c7f08
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-29 17:25:25 +0100

    add tags card, small changes to css

--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="d-flex">
-    <div class="container">
+    <div class="container ps-3 w-75">
         <div class="row gy-3">
             @foreach ($posts as $post)
                 <div class="col-md-4">
@@ -23,7 +23,7 @@
             </div>
         </div>
     </div>
-        <aside class="w-25 ms-3 me-auto">
+        <aside class="w-25 ms-3 me-auto pe-3">
             <div class="card">
                 <div class="card-body">
                     <h3 class="card-title">Categories:</h3>
@@ -33,6 +33,18 @@
                         <li class="list-group-item"><a href="{{route('categories.posts', $category->slug)}}">{{$category->name}}</a></li>
                     @empty
                         No categories found
+                    @endforelse
+                </ul>
+            </div>
+            <div class="card my-3">
+                <div class="card-body">
+                    <h3 class="card-title">Tags:</h3>
+                </div>
+                <ul class="list-group list-group-flush">
+                    @forelse ($tags as $tag)
+                        <li class="list-group-item"><a href="{{route('tags.posts', $tag->slug)}}">{{$tag->name}}</a></li>
+                    @empty
+                        No tags found
                     @endforelse
                 </ul>
             </div>
