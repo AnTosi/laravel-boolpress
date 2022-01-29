522818792f1800090037da1ffdb4c3ac3f07c671
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-29 17:45:49 +0100

    create admin tagcontroller

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $tags = Tag::all();
+        return view('admin.tags.index', compact('tags'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $validated_data = $request->validate([
+            'name' => 'required | unique:categories'
+        ]);
+        $validated_data['slug'] = Str::slug($request->name);
+
+        Tag::create($validated_data);
+
+        return redirect()->back()->with('feedback', 'Tag successfully created');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tag $tag)
+    {
+        //
+        $validated_data = $request->validate([
+            'name' => 'required | unique:categories'
+        ]);
+        $validated_data['slug'] = Str::slug($request->name);
+
+        $tag->update($validated_data);
+
+        return redirect()->back()->with('feedback', 'Tag successfully mopdified');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tag $tag)
+    {
+        //
+        $tag->delete();
+        return redirect()->back()->with('feedback', 'Tag successfully deleted');
+
+    }
+}
