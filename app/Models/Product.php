e2f69eac722a2b07fc09e712498b7eb5f8673ce1
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-30 11:10:10 +0100

    add belongs to user to model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Product extends Model
 {
     //
     protected $fillable = ['name', 'image', 'price', 'description'];
+
+    /**
+     * Get the user that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
