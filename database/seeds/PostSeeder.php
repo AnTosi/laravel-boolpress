78a45a710f806f925f1a00272f1946d78819631e
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-31 22:28:48 +0100

    add commented faker seed for placeholder image to be stored on db

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -20,6 +20,9 @@ class PostSeeder extends Seeder
             $post->title = $faker->sentence(3);
             $post->slug = Str::slug($post->title);
             $post->image = $faker->imageUrl(1200, 480, 'Posts', true, $post->title);
+
+            // This is to seed placeholders fakers image and save them in the storage, but i don't feel like fresh migrating everything and risk blowing up the database
+            // $post->image = 'placeholders/' . $faker->image('public/storage/placeholders', 1200, 480, 'Posts', false, true, $post->title);
             $post->sub_title = $faker->sentence(4);
             $post->body = $faker->paragraphs(10, true);
             $post->save();
