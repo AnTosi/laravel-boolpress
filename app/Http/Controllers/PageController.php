c4bdd31b132b57ab6928b76e0ba0709a3e0d8b69
AnTosi <andrea.tosi.uni@gmail.com>
2022-02-01 17:43:22 +0100

    add sendContactForm

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,4 +12,17 @@ class PageController extends Controller
         # code...
         return view('guest.contacts.form');
     }
+
+    public function sendContactForm(Request $request)
+    {
+        # code...
+        // ddd($request->all());
+        $validated_data = $request->validate([
+            'name' => 'required|min:4|max:50',
+            'email' => 'required|email',
+            'message' => 'required|min:10|max:500'
+        ]);
+
+        // ddd($validated_data);
+    }
 }
