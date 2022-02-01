35b3e385aa8a6b9c39866fbffb0ee94e63843562
AnTosi <andrea.tosi.uni@gmail.com>
2022-02-01 17:56:01 +0100

    add working form

--- resources/views/guest/contacts/form.blade.php
+++ resources/views/guest/contacts/form.blade.php
@@ -8,27 +8,28 @@
     </div>
 
     <div class="container">
-        <form action="" method="post">
+        @include('partials.error')
+        <form action="{{route('contacts.send')}}" method="post">
             @csrf
             <div class="row">
                 <div class="col">
                     <div class="mb-3">
                       <label for="name" class="form-label">Name</label>
-                      <input type="text" name="name" id="name" class="form-control" placeholder="Pippo Franco" aria-describedby="nameHelper" required minlength="4" maxlength="50">
+                      <input type="text" name="name" id="name" class="form-control" placeholder="Pippo Franco" aria-describedby="nameHelper" value='{{old('name')}}'required minlength="4" maxlength="50">
                       <small id="nameHelper" class="text-muted">Type your name here, maximum 50 characters</small>
                     </div>
                 </div>
                 <div class="col">
                     <div class="mb-3">
                         <label for="email" class="form-label">E-mail</label>
-                        <input type="email" name="email" id="email" class="form-control" placeholder="Pippo@Franco.com" aria-describedby="emailHelper" required>
+                        <input type="email" name="email" id="email" class="form-control" placeholder="Pippo@Franco.com" aria-describedby="emailHelper" value='{{old('email')}}' required>
                         <small id="emailHelper" class="text-muted">Type your email here</small>
                     </div>
                 </div>
 
                 <div class="mb-3">
                   <label for="message" class="form-label">Message</label>
-                  <textarea class="form-control" name="message" id="message" rows="4"></textarea>
+                  <textarea class="form-control" name="message" id="message" rows="4">{{old('message')}}'</textarea>
                 </div>
 
                 <div class="px-3">
