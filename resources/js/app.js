2ab778d4d554936b394e3b00168ebf9174ee069e
AnTosi <andrea.tosi.uni@gmail.com>
2022-02-02 19:07:25 +0100

    add axios call in js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+const { default: Axios } = require('axios');
+
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -21,12 +23,24 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
+ const app = new Vue({
     el: '#app',
+    data: {
+        posts: null
+    },
+    mounted() {
+        Axios.get('/api/posts').then(resp => {
+            console.log(resp);
+            this.posts = resp.data.data;
+        }).catch(e => {
+            console.error('Sorry! ' + e);
+        })
+    }
 });
