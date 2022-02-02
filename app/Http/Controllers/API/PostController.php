8c4bf1ea2b1e1bef637d2072b249d7279e349791
AnTosi <andrea.tosi.uni@gmail.com>
2022-02-02 16:39:49 +0100

    function in relation was plural

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
     public function index()
     {
         //
-        return Post::with(['tag'])->paginate(5);
+        return Post::with(['tags'])->paginate(5);
     }
 
     /**
