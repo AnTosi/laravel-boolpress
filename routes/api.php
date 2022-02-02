c1fef52249a45ba451cedaa1bf7a5f881c9d389e
AnTosi <andrea.tosi.uni@gmail.com>
2022-02-02 16:26:09 +0100

    test all api methods

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//api route "jsonification" without controller mode 1(good for plain php too), customizable
+// Route::get('posts', function(){
+//     //response->json
+//     $posts = Post::all();
+//     return response()->json([
+
+//         'response' => $posts
+//     ]);
+// });
+
+//api route "jsonification" without controller method 2 (laravel shortcut, only works with laravel , not in plain php)
+
+// Route::get('posts', function(){
+//     //response->json
+//     return Post::all();
+// });
+
+//api with relations and pagination
+// Route::get('posts', function(){
+//     //response->json
+//     return Post::with(['category'])->paginate(5);
+// });
+
+Route::get('posts', 'API\PostController@index');
