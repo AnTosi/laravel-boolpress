ef8a5863664da5606e18e218fb7d1b683e527536
AnTosi <andrea.tosi.uni@gmail.com>
2022-02-04 12:28:39 +0100

    fix axios call (was missing a .data), commented console log

--- resources/js/pages/Post.vue
+++ resources/js/pages/Post.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
+    <div class="container">
         <img :src="'/storage/' + post.image" alt />
-        <div class="card-body">
-            <h3>Cane {{ post.title }}</h3>
+        <div class="">
+            <h3>{{ post.title }}</h3>
+            <p> {{post.body}}</p>
+            <template v-if="post.category">Category: 
+                <p>@{{post.category.name}}</p>
+            </template>
+            <template v-if="post.tags">
+                Tags:
+                <p>
+                    <span v-for="tag in post.tags" :key="tag.slug">
+                    @{{tag.name}}
+                    </span>
+                </p>
+            </template>
         </div>
     </div>
 </template>
 
-<script>import Axios from "axios"
+<script>
+import Axios from "axios"
 
 export default {
 
@@ -17,11 +30,16 @@ export default {
         }
     },
     mounted() {
+        console.log('componente montato');
         // Call the api for the single post
         Axios.get('/api/posts/' + this.$route.params.slug).then(resp => {
-            console.log(this.$route.params.slug);
+            // console.log(this.$route.params.slug);
             console.log(resp);
-            this.post = resp.data;
+            this.post = resp.data.data;
+
+        })
+        .catch (error =>{
+            console.log('oops');
         })
     }
 
